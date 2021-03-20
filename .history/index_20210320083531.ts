6ee603c06e0e2f21b1a19cacf266d26221fb1d46
Calvin May <calvinkylemay@gmail.com>
2021-03-20 08:58:25 -0400

    Restructured project to work with Express

---
+++ .history/index_20210320083531.ts
@@ -0,0 +1,36 @@
+// Built in Modules
+import fs = require('fs');     // fs is now a reference to the built in fs module
+//const url = require('url');   // url is now a reference to the built in url module
+//const http = require('http'); // http is now a reference to the built in http module
+// Third Party Modules
+import express = require('express');
+
+
+const app = express();  // Use the express() constructor method to create an app
+const PORT: number = 3000;
+const HOST: string = "localhost";
+
+// Handles Routing
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+  });
+  
+// starts the Server and listens on PORT (3000)
+  app.listen(PORT, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
+
+// fs.readFile(file, function (err,data) 
+//     {
+//         if (err) 
+//         {
+//             res.writeHead(404);
+//             res.end("ERROR: 404 - Page not Found!");
+//             return;
+//         }
+//         res.setHeader("X-Content-Type-Options", "nosniff"); // Security: Avoids mime-sniffing
+//         let mime = lookup(path);
+//         res.writeHead(200, { "Content-type": mime });
+//         res.end(data);
+//     });
+    
--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 "use strict";
-
-
-// Built in Modules
-const fs = require('fs');     // fs is now a reference to the built in fs module
-//const url = require('url');   // url is now a reference to the built in url module
-//const http = require('http'); // http is now a reference to the built in http module
-// Third Party Modules
-const express = require('express');
-
-
-const app = express();  // Use the express() constructor method to create an app
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
 const PORT = 3000;
 const HOST = "localhost";
-
+loadStaticFiles();
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(PORT, () => {
+    displaySPA(res);
+});
+app.listen(PORT, () => {
     console.log(`Server running at http://${HOST}:${PORT}/`);
-  })
-
-// fs.readFile(file, function (err,data) 
-//     {
-//         if (err) 
-//         {
-//             res.writeHead(404);
-//             res.end("ERROR: 404 - Page not Found!");
-//             return;
-//         }
-//         res.setHeader("X-Content-Type-Options", "nosniff"); // Security: Avoids mime-sniffing
-//         let mime = lookup(path);
-//         res.writeHead(200, { "Content-type": mime });
-//         res.end(data);
-//     });
-    
+});
+function displaySPA(res) {
+    fs.readFile("index.html", (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end("ERROR: 404 - Page not Found!");
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+function loadStaticFiles() {
+    app.use(express.static(path.join(__dirname, "node_modules")));
+    app.use(express.static(path.join(__dirname, "Client")));
+    app.use(express.static(path.join(__dirname, "Views")));
+}
+//# sourceMappingURL=index.js.map
